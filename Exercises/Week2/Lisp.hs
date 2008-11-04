0947521dcbc97baca6d3c061e4ca0ff9aca33771
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2008-11-04 15:06:23 +0100

    small improvements

--- Exercises/Week2/Lisp.hs
+++ Exercises/Week2/Lisp.hs
@@ -1,4 +1,3 @@
-\begin{code}
 import ParserLibrary
 -- import Calculator hiding (symbol)
 import Test.QuickCheck
@@ -10,29 +9,24 @@ data SExp = Numb Int
           | Cons SExp SExp
             deriving (Show, Eq)
 
-\end{code}
-Make sure the code works for this examples...
-\begin{code}
 
 sample0 = Cons (Numb 2) (Atom "nil")
 sample1 = Cons (Numb 1) (Cons (Numb 2) (Atom "nil"))
-sample2 = Cons (Cons (Numb 2) (Atom "nil")) (Cons ((Numb 1)  (Atom "nil")))
+sample2 = Cons (Cons (Numb 2) (Atom "nil")) (Cons (Numb 1)  (Atom "nil"))
 
 -- 1.
 stringP p (Numb i) = show i
 stringP p (Atom s) = s
 stringP p (Cons h (Atom "nil")) = (if p then paren else id) (string h)
 stringP p (Cons h t) = (if p then paren else id) (string h ++ " " ++ stringP False t)
-\end{code}
- Note that the |p| parameter takes different values on left or right of |Cons|.
-\begin{code}
 
 string = stringP True
 
 paren x = "(" ++ x ++ ")"
 
+-- QC
 
-----------
+sexp = error "fill this is"
 
 arbitraryChar = elements (['a'..'z']++['0'..'9'])
 arbitraryString = oneof [return [], liftM2 (:) arbitraryChar arbitraryString]
@@ -47,4 +41,3 @@ instance Arbitrary SExp where
 prop_sexp_string s = take 1 (parse sexp (string s)) == [s]
 
 main = quickCheck prop_sexp_string
-\end{code}
