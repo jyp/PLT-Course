d780b8e9c33b22dea580a03f5735a427a300c19d
Richard Bubel <bubel@chalmers.se>
2011-08-15 13:33:32 +0200

    exam 2011 aug

--- Exercises/2010/Week5/rbTreeSolution.fs
+++ Exercises/2010/Week5/rbTreeSolution.fs
@@ -1,6 +1,6 @@
 module pp.rbtree
 
-type Color = 
+type Color =
     | Red 
     | Black
 
@@ -18,25 +18,25 @@ let rec isElement y tree =
     | Node(_, left, x, right) when x > y -> isElement y left 
 
 let balance col left el right =
-  match (col, left, el, right) with 
-  | (Black, Node(Red, Node(Red, a, x, b), y, c), z, d) 
-    | (Black, Node(Red, a, x, Node(Red, b, y, c)), z, d)
-    | (Black, a, x, Node(Red, Node(Red, b, y, c), z, d))
-    | (Black, a, x, Node(Red, b, y, Node(Red, c, z, d))) -> Node(Red, Node(Black, a, x, b),y,Node(Black, c, z, d)) 
-  | _ -> Node(col, left, el, right)
+    match (col, left, el, right) with 
+    | (Black, Node(Red, Node(Red, a, x, b), y, c), z, d) 
+        | (Black, Node(Red, a, x, Node(Red, b, y, c)), z, d)
+        | (Black, a, x, Node(Red, Node(Red, b, y, c), z, d))
+        | (Black, a, x, Node(Red, b, y, Node(Red, c, z, d))) -> Node(Red, Node(Black, a, x, b),y,Node(Black, c, z, d)) 
+    | _ -> Node(col, left, el, right)
 
 let insert x tree =
-   let rec ins tree =
-     match tree with
-     | Leaf  -> Node(Red, Leaf, x, Leaf)    
-     | Node(color, left, y, right) when x < y -> balance color (ins left) y right  
-     | Node(color, left, y, right) when x = y -> Node(color, left, y, right)
-     | Node(color, left, y, right) when x > y -> balance color left y (ins right)  
-   let makeBlack tree = 
-     match tree with
-     | Node(_,left, y, right) -> Node(Black, left, y, right)   (* the other case cannot happen *)
-   makeBlack (ins tree)
- 
+    let rec ins tree =
+        match tree with
+        | Leaf  -> Node(Red, Leaf, x, Leaf)    
+        | Node(color, left, y, right) when x < y -> balance color (ins left) y right  
+        | Node(color, left, y, right) when x = y -> Node(color, left, y, right)
+        | Node(color, left, y, right) when x > y -> balance color left y (ins right)  
+    let makeBlack tree = 
+        match tree with
+        | Node(_,left, y, right) -> Node(Black, left, y, right)   (* the other case cannot happen *)
+    makeBlack (ins tree)
+
 
 (* queries *)
 
@@ -55,10 +55,10 @@ let rec rbDepth(tree:RBTree) =
     match tree with
     | Leaf -> 1
     | Node(_, l, _, r) -> 
-       let leftDepth  = rbDepth l
-       let rightDepth = rbDepth r 
-       if leftDepth > rightDepth then leftDepth + 1
-       else rightDepth + 1
+        let leftDepth  = rbDepth l
+        let rightDepth = rbDepth r 
+        if leftDepth > rightDepth then leftDepth + 1
+        else rightDepth + 1
 
 
 (* tests *)
@@ -84,14 +84,18 @@ tree3: rbSum: 50005000; rbDepth: 19 rbSize: 10000
 
 (* asynchronous *)
 
-let compDepth tree = async { return rbDepth tree } 
-let compSize  tree = async { return rbSize tree } 
-let compSum   tree = async { return rbSum tree } 
+let compDepth tree = async {
+    let depth = rbDepth tree
+    } 
+let compSize  tree = async { let size  = rbSize tree
+                             size } 
+let compSum   tree = async { let sum   = rbSum tree
+                             sum } 
+
 
 
 
 let computeAll tree = Async.Parallel[compDepth tree; compSize tree; compSum tree]
-
 
 (* The following line, best to show in fsi
 
@@ -99,27 +103,28 @@ Async.RunSynchronously (computeAll tree3) ;;
 
 *)
 
+Async.RunSynchronously (computeAll tree3)
 
 
 (* immutable set *)
 type ImmutableIntSet(tree:RBTree) = 
-   let rbBacked = tree
-   new() = new ImmutableIntSet(empty)
+    let rbBacked = tree
+    new() = new ImmutableIntSet(empty)
 
-   member v.Size() = rbSize rbBacked
-   member v.Depth() = rbDepth rbBacked
-   member v.HasElement(el:int) = isElement el rbBacked 
-   member v.Add(el:int) = new ImmutableIntSet(insert el rbBacked)
+    member v.Size() = rbSize rbBacked
+    member v.Depth() = rbDepth rbBacked
+    member v.HasElement(el:int) = isElement el rbBacked 
+    member v.Add(el:int) = new ImmutableIntSet(insert el rbBacked)
 
 (* mutable set *)
 type MutableIntSet(tree:RBTree) = 
-   let mutable rbBacked = tree
-   new() = new MutableIntSet(empty)
+    let mutable rbBacked = tree
+    new() = new MutableIntSet(empty)
 
-   member v.Size() = rbSize rbBacked
-   member v.Depth() = rbDepth rbBacked
-   member v.HasElement(el:int) = isElement el rbBacked 
-   member v.Add(el:int) = rbBacked <- insert el rbBacked
+    member v.Size() = rbSize rbBacked
+    member v.Depth() = rbDepth rbBacked
+    member v.HasElement(el:int) = isElement el rbBacked 
+    member v.Add(el:int) = rbBacked <- insert el rbBacked
 
 
 (* test *)
