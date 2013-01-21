b33354671bf907809a58797ab6949c5ade06d879
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2013-01-21 09:59:02 +0100

    notes in lecturing

--- Pub/GroupAssignment.hs
+++ Pub/GroupAssignment.hs
@@ -5,11 +5,11 @@ xor True  x = not x
 bits 0 = []
 bits n = (n `mod` 2 /= 0) : bits (n `div` 2)
 
-show True = "Group A"
-show False = "Group B"
+sho True = "Group A"
+sho False = "Group B"
 
 group = foldr xor False . bits 
 
 myPersonNumber = 197812150666
-myGroup = group myPersonNumber
+myGroup = sho $ group myPersonNumber
 
