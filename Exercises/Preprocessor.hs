9610cf742b04d09af74dc9c077674bb96af4bb3b
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2013-01-23 16:12:02 +0100

    preprocessor to remove answers. Curse you, Don Knuth.

---
+++ Exercises/Preprocessor.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE RecordWildCards #-}
+  
+  module Main where
+
+import Data.Maybe
+
+data Status = Status {transmit :: Maybe String, inAnswer :: Bool, answersDeactivated :: Bool}
+
+updStatus Status {..} s
+  | s == "\\begin{ans}" && answersDeactivated = Status {transmit = Nothing, inAnswer = True, ..}
+  | s == "\\end{ans}"   && answersDeactivated = Status {transmit = Nothing, inAnswer = False, ..}
+  | s == "\\DeactivateAnswers" = Status {transmit = Nothing, answersDeactivated = True, ..}
+  | inAnswer = Status {transmit = Nothing, ..}
+  | otherwise = Status {transmit = Just s, ..}
+
+main = do
+  x <- lines `fmap` readFile "All.tex"
+  putStrLn . unlines . catMaybes . map transmit $ scanl updStatus (Status Nothing False False) $ x
+  
+  
