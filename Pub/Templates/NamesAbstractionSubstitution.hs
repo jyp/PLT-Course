f2076e4e2ddafea3d48b2438ee53bd9254448872
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-01-22 16:45:26 +0100

    notes taken during lecture 2

--- Pub/Templates/NamesAbstractionSubstitution.hs
+++ Pub/Templates/NamesAbstractionSubstitution.hs
@@ -5,4 +5,4 @@ ringArea = 30 * 30 * 3.141592 - 20 * 20 * 3.141592
 -- what can be named?
 -- what can be abstracted over?
 -- recover the original by substitution.
--- pitfall: name capture.
+-- pitfall: name capture. (and other kind of side effects!)
