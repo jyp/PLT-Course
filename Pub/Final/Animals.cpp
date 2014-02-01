48e1e05f5a5ced4aea57f33dc922a287f25cd065
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-02-01 10:30:31 +0100

    final animals

--- Pub/Final/Animals.cpp
+++ Pub/Final/Animals.cpp
@@ -7,21 +7,30 @@ public:
   }
 };
 
-class Cat : public Animal {
-  int x;
-  virtual void sound() {
-    x++;
-    printf("meow %d\n",x);
-  }
-};
-
 class Dog : public Animal {
+public:
   virtual void sound() {
-    printf("woof\n");
+    printf("woof!!!\n");
   }
 };
 
+class Cat : public Animal {
+private:
+  int times;
+public:
+  virtual void sound() {
+    // super.sound();
+    times++;
+    printf("meow %d\n", times);
+  }
+  Cat () {
+    times = 0;
+  }
+};
 
+// Make two derived classes; at least one should have a state.
+
+// Test code:
 void test(Animal* a) {
   a->sound();
 }
@@ -31,7 +40,8 @@ void test2(Animal a) {
 }
 
 main() {
-  Cat a;
-  // test(&a);
+  Cat a; //TODO: Test on the derived class
   test2(a);
+  test2(a);
+  // test2(a);
 }
--- Pub/Final/AnimalsRec.cpp
+++ Pub/Final/AnimalsRec.cpp
@@ -4,55 +4,75 @@ struct Animal {
   void (*sound)(Animal*);
 };
 
-void animal_sound(Animal* a) {
+void animal_sound(Animal* this_) {
   printf("huh?\n");
 }
 
+// Pay attention: constructor needs to set the method pointers.
 struct Animal construct_animal() {
-  Animal a = {animal_sound};
+  // TODO
+  Animal a;
+  a.sound = animal_sound;
   return a;
 }
 
+////////////////
+
 struct Cat {
-  void (*sound)(Cat*);
-  int x;
+  void (*sound)(Cat *);
+  int times;
 };
 
-void cat_sound(Cat* a) {
-  printf("meow %d\n",a->x);
+void cat_sound(Cat* this_) {
+  animal_sound(reinterpret_cast<Animal*>(this_));
+  this_->times++;
+  printf("meow %d\n", this_->times);
 }
 
+// Pay attention: constructor needs to set the method pointers.
 struct Cat construct_cat() {
-  Cat c = {cat_sound, 87654};
-  return c;
+  // TODO
+  Cat a;
+  a.sound = cat_sound;
+  a.times = 0;
+  return a;
 }
+
+////////////////
 
 struct Dog {
-  void (*sound)(Dog*);
+  void (*sound)(Dog *);
 };
 
-void dog_sound(Dog* a) {
-  printf("woof\n");
+void dog_sound(Dog* this_) {
+  printf("woof!!!\n");
 }
 
+// Pay attention: constructor needs to set the method pointers.
 struct Dog construct_dog() {
-  Dog d = {dog_sound};
-  return d;
+  // TODO
+  Dog a;
+  a.sound = dog_sound;
+  return a;
 }
 
+// In derived classes, added fields should come after.
+
+// Translate test code.
+
+
+// Test code:
 void test(Animal* a) {
-  a->sound(a);  // (1)
+  a->sound(a);
 }
 
 void test2(Animal a) {
   animal_sound(&a);
-  // a.sound(&a); // This may cause crashes!
- }
+}
 
 main() {
-  Cat c = construct_cat();
-  test((Animal*)&c); // (2)
-  Animal a = construct_animal();
-  test2(a);
-  // test2(*reinterpret_cast<struct Animal*>(&c));
+  Cat a = construct_cat(); //TODO: Test on the derived class
+  // test(reinterpret_cast<Animal*>(&a));
+  test2(*reinterpret_cast<Animal*>(&a));
 }
+
