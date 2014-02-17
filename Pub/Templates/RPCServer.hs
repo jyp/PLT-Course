ce7f9f238c5a71ee06ef41f1cc7ce21a47ec757e
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-02-17 09:32:52 +0100

    prepare lecture CP 1

--- Pub/Templates/RPCServer.hs
+++ Pub/Templates/RPCServer.hs
@@ -1,19 +1,26 @@
+module RPCS where
+
 import Control.Concurrent
-import Control.Concurrent.Chan
 
 data Query = Call Int (Chan Int)
 
+-- Idea: instead of computing the function locally, have another
+-- process running it (in reality perhaps on another, faster machine,
+-- or one that has data that we don't want to transfer to the client)
 serverFunction n = product [1..n]
 
-server :: Chan Query -> IO ()
+-- API
+-- createServer :: IO (Chan Query)
+-- remoteCall :: Chan Query -> Int -> IO Int
 
-createServer :: IO (Chan Query)
-
-remoteCall :: Chan Query -> Int -> IO Int
-
+-- Example usage:
 -- main = do
 --   c <- createServer
 --   remoteCall c 5
+
+-- Implementation:
+-- server :: Chan Query -> IO ()
+
 
 
 
