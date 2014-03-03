b5c99c0f87f80f1ee5589e3d1c529941d64e171d
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-03-03 09:28:05 +0100

    KAM template

--- Pub/Final/KAM.hs
+++ Pub/Final/KAM.hs
@@ -1,18 +1,15 @@
-import Data.Tree
+module KAM where
+
 import Text.Show
-import Control.Arrow
 import Data.List
 import Prelude hiding (succ)
 
 type Sym = String
-data Term = Var Sym | Lam Sym Term | App Term Term | Con String
+data Term = Var Sym | Lam Sym Term | App Term Term
 --  deriving Show
 
-parens s = "("++s++")"
-
 instance Show Term where
-    showsPrec d (Con x) = showString x
-    showsPrec d (Var x) = showString x
+    showsPrec _ (Var x) = showString x
     showsPrec d (Lam x t) = showParen (d > 0) (showString "\\" . showString x . showString "->" . showsPrec 0 t)
     showsPrec d (App t1 t2) = showParen (d > 1) (showsPrec 1 t1 . showString " " . showsPrec 2 t2)
 
@@ -26,6 +23,7 @@ lookupEnv :: Sym -> Env -> Closure
 lookupEnv x [] = error $ x ++ " not found in env!"
 lookupEnv x ((y,v):rho) = if x == y then v else lookupEnv x rho
 
+eval :: Closure -> Stack -> Term
 eval (Var x    :+ rho) s   = case lookup x rho of
   Just v -> eval v s
   Nothing -> foldl App (Var x) (map (flip eval []) s)
@@ -47,7 +45,6 @@ twice = Lam "f" $ Lam "x" $ (Var "f" `App` ( Var "f" `App` Var "x"))
 _id = Lam "y" $ Var "y"
 
 value1 = twice `App` _id
-value2 = (twice `App` _id) `App` (Con "V")
 
 
 delta = Lam "x" $ App (Var "x") (Var "x") 
