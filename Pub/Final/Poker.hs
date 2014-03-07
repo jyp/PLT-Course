f6c70f60554e521f6e8e30458c690a094da17196
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2014-03-07 08:09:13 +0100

    exam preparation

--- Pub/Final/Poker.hs
+++
@@ -1,7 +0,0 @@
-
-data Value = A | K | Q | N | Ten | Nine | Eight | Seven | Six | Five | Four | Three | Two
-
-type Hand = [Value]
-
-fourOfAKind hand | hand =:= x++y:z & (x++z) =:= [r,r,r,r] = r
-  where x,y,z,r free
