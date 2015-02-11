e45f7ec1c37104ce78512193c34342aa93e87dc0
Jean-Philippe Bernardy <jeanphilippe.bernardy@gmail.com>
2015-02-11 09:48:04 +0100

    sync

--- Pub/Templates/HOT.hs
+++ Pub/Templates/HOT.hs
@@ -2,13 +2,13 @@
 
 type Point = (Float,Float)
 type Region = ?
-
+ 
 -- (.+.) :: Point -> Point -> Point
 -- opposite :: Point -> Point
 -- (.-.) :: Point -> Point -> Point
 -- norm2 :: Point -> Float
 -- outside :: Region -> Region
--- (∧) :: Region -> Region -> Region
+-- (∩) :: Region -> Region -> Region
 -- within :: Float -> Point -> Region
 -- engagementZone :: Point -> Region
 
